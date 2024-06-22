16b0773882c384dc3f99da15428c5d88bb4b8504
yieniggu <bastianandresxb@gmail.com>
2024-06-22 13:32:49 -0400

    refactor: now save frames instead of time of event occuring

--- yolotrack_csv_only.py
+++ yolotrack_csv_only.py
@@ -14,10 +14,6 @@ initial_time = datetime.datetime(2024, 5, 14, 12, 0, 0)
 model = YOLO('mut_v2.pt')  # Load an official Detect model
 byte_tracker = sv.ByteTrack()
 
-video_result = cv2.VideoWriter('mut_results.avi',  
-                         cv2.VideoWriter_fourcc(*'MJPG'), 
-                         22, (1024, 768)) 
-
 cap = cv2.VideoCapture("./test_videos/pizza_test.mp4")
 if (cap.isOpened() == False):
     print('!!! Unable to open URL')
@@ -48,8 +44,6 @@ total_in = total_out = 0
 frame_count = 1
 fps = FPS().start()
 while(cap.isOpened()):
-	# time when we finish processing for this frame 
-	new_frame_time = time.time() 	
 	ret, frame = cap.read()
 
 	if not ret:
@@ -63,17 +57,6 @@ while(cap.isOpened()):
 	detections = byte_tracker.update_with_detections(detections)
 
 	labels = [f"#{tracker_id}" for tracker_id in detections.tracker_id]
-	
-	annotated_frame = bounding_box_annotator.annotate(
-		scene=frame.copy(),
-		detections=detections
-	)
-	annotated_frame = bounding_box_annotator.annotate(
-        scene=frame.copy(), detections=detections)
-	# annotated_frame = label_annotator.annotate(
-    #     scene=annotated_frame, detections=detections, labels=labels)
-
-	annotated_frame = sv.draw_line(annotated_frame, start, end, sv.Color(0, 255, 50))
 
 	crossed_in, crossed_out = line_zone.trigger(detections)
 	if np.any(crossed_in):
@@ -85,27 +68,7 @@ while(cap.isOpened()):
 		delivered.append(["delivered", frame_count])
 		total_out += 1
 
-	annotated_frame = cv2.putText(annotated_frame, "TOTAL DELIVERED:{}".format(total_out),
-                                                (5, 50), cv2.FONT_HERSHEY_SIMPLEX , 0.6, (0, 255, 0), 2, cv2.LINE_AA)
-	annotated_frame = cv2.putText(annotated_frame, "TOTAL RETURNED:{}".format(total_in),
-                                                (5, 70), cv2.FONT_HERSHEY_SIMPLEX , 0.6, (0, 0, 255), 2, cv2.LINE_AA)
-	annotated_frame = cv2.putText(annotated_frame, "TOTAL:{}".format(total_out-total_in),
-                                                (5, 90), cv2.FONT_HERSHEY_SIMPLEX , 0.6, (255, 0, 0), 2, cv2.LINE_AA)
-            
-	fps.update()
-	fps.stop()
-	print("FPS: {:.2f}".format(fps.fps()))
-
-	fps_text = "{0:.2f}".format(fps.fps())
-	cv2.putText(annotated_frame, "FPS: " + fps_text, (5, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-	video_result.write(annotated_frame) 
-
 	frame_count += 1
-
-	cv2.imshow('frame', annotated_frame)
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-	 	break
 
 df = pd.DataFrame(delivered, columns=["delivered_event", "delivered_at_frame"])
 df.to_csv("delivered_results.csv", index=False)
@@ -114,5 +77,4 @@ df2 = pd.DataFrame([frame_count], columns=["total_frames"])
 df2.to_csv("total_frames.csv", index=False)
 
 cap.release()
-video_result.release()
 cv2.destroyAllWindows()
